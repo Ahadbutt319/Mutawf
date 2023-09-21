cf9b561fde96b24a5c52a2f6dc7274c8c7484c40
Rahmat <rehmatullah@vigoroustechnologies.com>
2023-09-21 18:44:26 +0500

    installed google translator

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -4,18 +4,21 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+// use GoogleTranslate;
 
 class CompanyController extends Controller
 {
     public function store(Request $request)
     {
         $rules = [
-            'name_en' => 'required',
-            'name_ar' => 'required',
-            'description_en' => 'required',
-            'description_ar' => 'required',
+            'name' => 'required',
+            'description' => 'required',
             'type' => 'nullable|max:255',
             'founded'
         ];
+
+        //  Translation Example
+        // $translatedText = GoogleTranslate::translate('Hello, world!', 'en', 'es');
+        // $translatedText = GoogleTranslate::translate(['Hello, world!', 'Goodbye, world!'], 'en', 'es');
     }
 }
